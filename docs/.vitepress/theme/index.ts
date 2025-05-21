46f697ea5a3bb1d47a8f2b1b8410e146841d3584
edboak <oakbarrett+edb@gmail.com>
2025-05-21 16:58:31 -0700

    Update index.ts
    uncommenting the custom.css

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,7 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-// import './custom.css'
+import './custom.css'
 
 export default {
   extends: DefaultTheme,
